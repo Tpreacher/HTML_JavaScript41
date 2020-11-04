d9efdc3ea3f4df7ff084e936a45a6b3e2730f6e9
Tpreacher <72519364+Tpreacher@users.noreply.github.com>
2020-11-04 16:06:04 -0500

    end of class week 5

---
+++ HTML_Javascript_41/Week5Lab/Car.js
@@ -0,0 +1,20 @@
+var randomNumber = Math.random() * 10;
+//Math.random() * (high-low) + low
+alert(randomNumber);
+
+function randomRange(high, low){
+    return Math.round(Math.random()*(high - low) + low)
+}
+
+function GameObject(){
+    this.x = 50;
+    this.y = 200;
+    this.w = 50;
+    this.h = 50;
+    this.color = "purple";
+
+    this.draw = function(){
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x,this.y,this.w,this.h)
+    }
+}
---
+++ HTML_Javascript_41/Week5Lab/scriptCarGame.js
@@ -0,0 +1,153 @@
+//JavaScript here
+var c = document.querySelector('canvas');
+var ctx = c.getContext('2d');
+
+var Yoshi = new Image();
+Yoshi.src = 'images/Yoshi.png';
+
+var x = 0;
+
+//Timer
+var timer = requestAnimationFrame(main);
+
+//Fuel Varaibles
+var startFuel = 610;
+var fuel = startFuel;
+var barFullWidth = 300;
+
+//Start timer Stuff
+var sec = 3;
+var fps = 60;
+var frames = fps;
+
+var start = 110;
+var finish = 700;
+
+var car = new GameObject();
+car.y = c.height/2;
+car.w = 100;
+car.x = 5;
+car.color = "green";
+
+var car2 = new GameObject();
+car2.y = 400;
+
+var car3 = new GameObject();
+car3.y = 500;
+
+function main() {
+    timer = requestAnimationFrame(main);
+    //Clear Canvas
+    ctx.clearRect(0, 0, 800, 600);
+    //draw the game objects
+    //drawStartLine();
+    drawBox("red", start, 100, 10 ,400);
+    //drawFinishLine();
+    drawBox("green", finish, 100, 10, 400);
+    //drawBox();
+    drawSprite();
+    drawFuelBar();
+    drawFuelText();
+  
+
+    if(sec > 0){
+        
+        runStartTimer();
+        drawStartTimer();
+    }  
+  
+    else{
+        if(fuel > 0){
+            //update x
+            car.x += 1;
+            x += 1;
+            fuel -=1;
+        }
+    } 
+    car.draw();
+    car2.draw();
+    car3.draw();
+
+     //Draw some text
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+    ctx.font = "50px Arial";
+    ctx.textAlign = 'center';
+    ctx.fillText("Week 4 Lab", c.width / 2, 50);
+    ctx.strokeText("Week 4 Lab", c.width / 2, 50);
+
+    //checks to see if player made it to finish line
+    if(fuel<= 0 || x + 100 > finish){
+        drawResults();
+    }
+ 
+}
+
+function drawBox() {
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(x, c.height/ 2, 100, 50);
+}
+
+function drawSprite() {
+    //draw an imcage to canvas
+    ctx.drawImage(Yoshi, x, 100, 100, 100);
+}
+
+function drawStartLine() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(start, 100, 10, 400);
+}
+
+function drawFinishLine() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(finish, 100, 10, 400);
+}
+
+function drawFuelBar() {
+    var barCurrentWidth = barFullWidth * getFuelPercentage();
+
+    ctx.fillStyle = 'mustard';
+    ctx.fillRect(start, 80, barCurrentWidth, 10);
+}
+
+function drawFuelText(){
+    ctx.fillStyle = 'black';
+    ctx.font = '30px Arial';
+    ctx.fillText(fuel, start, 50)
+}
+
+function getFuelPercentage(){
+    return fuel/startFuel;
+}
+
+function drawResults(){
+    if(x + 100 > finish){
+        //Winning Condition
+        ctx.fillStyle = 'black';
+        ctx.font = '30px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText("You made it to the Finish Line! You Won!", c.width/2, c.height/2);
+    }
+    else{
+        //Losing Condition
+        ctx.fillStyle = 'black';
+        ctx.font = '30px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText("You ran out of fuel....whomp whommp :( You Lose", c.width/2, c.height/2);
+    }
+}
+
+function runStartTimer(){
+    frames -= 1;
+    if(frames < 0){
+        frames = fps;
+        sec-= 1;
+    }
+}
+
+function drawStartTimer(){
+    ctx.fillStyle = 'black';
+    ctx.font = '30px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText(sec, c.width / 2, c.height / 2);
+}
